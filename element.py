2d8a4fc7bdbf824c196acf8a4b691098617d242d
ProfessionalDebil <71475850+ProfessionalDebil@users.noreply.github.com>
2023-01-06 20:58:49 +0700

    Add Element Data and Molecule

--- element.py
+++ element.py
@@ -1,2 +1,22 @@
 class Element:
+    """
+    This is the class for Elements such as H, Fe, and O
+
+    Attributes:
+    number : int
+        The atomic number of the element
+    name : str
+        The full name of the element
+    symbol : str
+        The symbol of the element
+    mass : float
+        The atomic mass of the element
+
+    Methods:
+    """
     
+    def __init__(self, number: int, name: str, symbol: str, mass: float):
+        self.number = number
+        self.name = name
+        self.symbol = symbol
+        self.mass = mass
